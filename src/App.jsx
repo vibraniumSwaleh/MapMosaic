c9b5787144c4cb66cf8c8a8af7025d129b7599dc
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-02 21:14:16 +0000

    extract citie state into a custom context provider

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,7 @@ import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
 
-const URL = 'http://localhost:9000/';
-
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${URL}cities`);
-        const data = await res.json();
-
-        setCities(data);
-        console.log(data);
-      } catch (error) {
-        alert('There was an error loading data');
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
@@ -45,15 +22,9 @@ function App() {
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
           <Route index element={<Navigate replace to='cities' />} />
-          <Route
-            path='cities'
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route path='cities' element={<CityList />} />
           <Route path='cities/:id' element={<City />} />
-          <Route
-            path='countries'
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          />
+          <Route path='countries' element={<CountryList />} />
           <Route path='form' element={<Form />} />
         </Route>
         <Route path='*' element={<PageNotFound />} />
