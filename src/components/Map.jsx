357cb715a7dcb6776afdba3f2c04e6aa69ae0d2b
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-03 23:28:21 +0000

    center map on city click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,37 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import { useCities } from '../contexts/CitiesContext';
+import { useEffect, useState } from 'react';
 
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
-  const [searchParams, setSearchParams] = useSearchParams();
-  // const lat = searchParams.get('lat');
-  // const lng = searchParams.get('lng');
+  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+  const mapLat = searchParams.get('lat');
+  const mapLng = searchParams.get('lng');
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng],
+  );
+
+  function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+  }
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={mapPosition}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
+        <ChangeCenter position={mapPosition} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
@@ -29,8 +43,8 @@ function Map() {
               position={[city.position.lat, city.position.lng]}
             >
               <Popup>
-                {city.country} {city.emoji}
-                <br /> {city.cityName}.
+                <span>{city.emoji}</span>
+                <span> {city.cityName}</span>
               </Popup>
             </Marker>
           );
