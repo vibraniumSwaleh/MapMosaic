dbe7b42d1f7917fdc8c85addba4ab85816d3d58b
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-06 21:00:16 +0000

    complete Authentication Context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,12 +1,54 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.payload) {
+    case 'login':
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case 'logout':
+      return { ...initialState };
+    default:
+      break;
+  }
+}
+
+const USER = {
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz',
+};
+
 function AuthProvider({ children }) {
-  return <AuthContext.Provider value={''}>{children}</AuthContext.Provider>;
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  function login(email, password) {
+    if (email === USER.email && password === USER.password)
+      dispatch({ type: 'login', payload: USER });
+  }
+  function logout() {
+    dispatch({ type: 'logout' });
+  }
+
+  return (
+    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
   const context = useContext(AuthContext);
   if (!context) throw new Error('AuthContext was used outsode of AuthProvider');
 }
+
+export { AuthProvider, useAuth };
