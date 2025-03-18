d84abcfce75377838529569ebbd20c4e7d195bba
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-03-18 00:08:52 +0000

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ import Button from '../components/Button';
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState('jack@example.com');
+  const [email, setEmail] = useState('john.doe@example.com');
   const [password, setPassword] = useState('qwerty');
 
   const { login, isAuthenticated } = useAuth();
